5618ea8eb918dbf07ff92a283a5a7737a272fb8c
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-05-15 10:19:55 -0700

    Revised without class BSTIterator

--- Set.py
+++ Set.py
@@ -86,7 +86,7 @@ class BSTNode:
 
 
 
-class BSTSet:
+class Set:
     def __init__(self, get_key_function=None):
         self.storage_root = None
         self.get_key = get_key_function if get_key_function else lambda el: el
